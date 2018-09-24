cdde805430d2cc6ace646cca576545ce78c96c92
simontuo <simontuo666@gmail.com>
2018-09-24 11:30:05 +0000

    用户界面 - 收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ Auth::routes();
 
 Route::get('/', 'PagesController@root')->name('root');
 Route::get('/products', 'ProductsController@index')->name('products.index');
-Route::get('/products/{product}', 'ProductsController@show')->name('products.show');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
@@ -31,5 +30,9 @@ Route::group(['middleware' => 'auth'], function () {
         Route::delete('/user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
         Route::post('/products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
         Route::delete('/products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+        Route::get('/products/favorites', 'ProductsController@favorites')->name('products.favorites');
     });
 });
+
+Route::get('/products/{product}', 'ProductsController@show')->name('products.show');
+
