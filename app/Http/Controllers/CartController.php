92a2893dee9985aab32e1733ff086497b93c1458
simontuo <simontuo666@gmail.com>
2018-10-02 03:22:13 +0000

    购物车页面

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AddCartRequest;
 use App\Models\CartItem;
+use App\Models\ProductSku;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
@@ -26,6 +27,20 @@ class CartController extends Controller
         $cart->user()->associate($user);
         $cart->productSku()->associate($skuId);
         $cart->save();
+
+        return [];
+    }
+
+    public function index(Request $request)
+    {
+        $carItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+
+        return view('cart.index', ['cartItems' => $carItems]);
+    }
+
+    public function remove(ProductSku $sku, Request $request)
+    {
+        $request->user()->cartItems()->where('product_sku_id', $sku->id)->delete();
 
         return [];
     }
