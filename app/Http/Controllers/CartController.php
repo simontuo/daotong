7de668c4c7f695900ee44261d76ea722bf24081e
simontuo <simontuo666@gmail.com>
2018-10-06 16:01:52 +0000

    创建订单

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -34,8 +34,9 @@ class CartController extends Controller
     public function index(Request $request)
     {
         $carItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+        $addresses = $request->user()->addresses()->orderBy('last_used_at', 'desc')->get();
 
-        return view('cart.index', ['cartItems' => $carItems]);
+        return view('cart.index', ['cartItems' => $carItems, 'addresses' => $addresses]);
     }
 
     public function remove(ProductSku $sku, Request $request)
