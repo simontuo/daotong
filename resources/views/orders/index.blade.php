3a6386d6047373275877691ea39f2f5966cf4310
simontuo <simontuo666@gmail.com>
2018-10-06 16:46:13 +0000

    用户界面 - 订单详情页

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -37,10 +37,10 @@
                                                             </a>
                                                         </div>
                                                         <div>
-                    <span class="product-title">
-                       <a target="_blank"
-                          href="{{ route('products.show', [$item->product_id]) }}">{{ $item->product->title }}</a>
-                    </span>
+                                                            <span class="product-title">
+                                                                <a target="_blank"
+                                                                    href="{{ route('products.show', [$item->product_id]) }}">{{ $item->product->title }}</a>
+                                                            </span>
                                                             <span class="sku-title">{{ $item->productSku->title }}</span>
                                                         </div>
                                                     </td>
@@ -66,8 +66,9 @@
                                                                 否则订单将自动关闭
                                                             @endif
                                                         </td>
-                                                        <td rowspan="{{ count($order->items) }}" class="text-center"><a
-                                                                    class="btn btn-primary btn-xs" href="">查看订单</a></td>
+                                                        <td rowspan="{{ count($order->items) }}" class="text-center">
+                                                            <a class="btn btn-primary btn-xs" href="{{ route('orders.show', ['order' => $order->id]) }}">查看订单</a>
+                                                        </td>
                                                     @endif
                                                 </tr>
                                             @endforeach
