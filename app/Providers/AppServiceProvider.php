54006d4fffd1158822e126299ba83e8754d1c641
simontuo <simontuo666@gmail.com>
2018-10-07 04:39:55 +0000

    订单支付

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,11 +26,14 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         // 往服务容器中注入一个名为 alipay 的单例对象
-        $this->app->singleton('alipay', function() {
-            $config = config('pay.alipay');
+        $this->app->singleton('alipay', function () {
+            $config               = config('pay.alipay');
+//            $config['notify_url'] = route('payment.alipay.notify');
+            $config['notify_url'] = 'http://requestbin.fullcontact.com/ve3xeeve';
+            $config['return_url'] = route('payment.alipay.return');
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
-                $config['mode'] = 'dev';
+                $config['mode']         = 'dev';
                 $config['log']['level'] = Logger::DEBUG;
             } else {
                 $config['log']['level'] = Logger::WARNING;
@@ -39,15 +42,15 @@ class AppServiceProvider extends ServiceProvider
             return Pay::alipay($config);
         });
 
-        $this->app->singleton('wechat_pay', function() {
+        $this->app->singleton('wechat_pay', function () {
             $config = config('pay.wechat');
-           if (app()->environment() !== 'production') {
-               $config['log']['level'] = Logger::DEBUG;
-           } else {
-               $config['log']['level'] = Logger::WARNING;
-           }
+            if (app()->environment() !== 'production') {
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
             // 调用 Yansongda\Pay 来创建一个微信支付对象
-           return Pay::wechat($config);
+            return Pay::wechat($config);
         });
     }
 }
