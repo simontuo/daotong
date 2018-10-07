53bcfc7224267b9b12dc561ac0fec6c89fb0e389
simontuo <simontuo666@gmail.com>
2018-10-07 05:31:13 +0000

    完善支付后逻辑

---
+++ app/Listeners/SendOrderPaidMail.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\OrderPaid;
+use App\Notifications\OrderPaidNotification;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+// implements ShouldQueue 代表异步监听器
+class SendOrderPaidMail implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  OrderPaid  $event
+     * @return void
+     */
+    public function handle(OrderPaid $event)
+    {
+        $order = $event->getOrder();
+
+        $order->user->notify(new OrderPaidNotification($order));
+    }
+}
