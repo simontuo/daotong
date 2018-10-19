a6c1449892921f86cf35ea2b344868a6fe37d686
simontuo <simontuo666@gmail.com>
2018-10-19 15:59:57 +0000

    管理后台 - 优惠券列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -19,7 +19,8 @@ Route::group([
     $router->put('/products/{id}', 'ProductsController@update');
     $router->get('/orders', 'OrdersController@index')->name('admin.orders.index');
     $router->get('/orders/{order}', 'OrdersController@show')->name('admin.orders.show');
-    $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
-    $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('admin.orders.handle_refund');
+    $router->post('/orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship');
+    $router->post('/orders/{order}/refund', 'OrdersController@handleRefund')->name('admin.orders.handle_refund');
+    $router->get('/coupon_codes', 'CouponCodesController@index');
 
 });
