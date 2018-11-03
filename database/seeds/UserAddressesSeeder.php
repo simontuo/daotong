704e0c74f9caee9fc91bd073c4dd943c60aafa17
simontuo <simontuo666@gmail.com>
2018-11-03 03:10:01 +0000

    完善假数据填充

---
+++ database/seeds/UserAddressesSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\User;
+use App\Models\UserAddress;
+use Illuminate\Database\Seeder;
+
+class UserAddressesSeeder extends Seeder
+{
+    /**
+     * @throws Exception
+     */
+    public function run()
+    {
+        User::all()->each(function (User $user) {
+            factory(UserAddress::class, random_int(1, 3))->create(['user_id' => $user->id]);
+        });
+    }
+}
